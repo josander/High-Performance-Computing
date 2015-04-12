4d1eccf4fefcfcb9a45ff2e1499974e9672ed877
Josephine <jossan_cuellar@hotmail.com>
2015-04-12 14:12:44 +0200

    Two more tasks

--- Assignment1.m
+++ Assignment1.m
@@ -1,4 +1,5 @@
 %% Matlab-assignment
+% Dynamic memory allocation
 
 clc
 clear all
@@ -7,50 +8,50 @@ n = 1000;
 p = 2000;
 x = rand(n, 1);
 
-tic 
-    X = zeros(n, p);
-    for k = 1:p
-        X(:, k) = x;
-    end
+tic
+X = zeros(n, p);
+for k = 1:p
+    X(:, k) = x;
+end
 toc
 
 tic
-    X = [];
-    for k = 1:p
-        X(:, k) = x;
-    end
+X = [];
+for k = 1:p
+    X(:, k) = x;
+end
 toc
 
 tic
-    X = [];
-    for k = 1:p
-        X = [X, x];
-    end
+X = [];
+for k = 1:p
+    X = [X, x];
+end
 toc
 
 %--------------------------------------------------------------------------
 
 x = x';
 
-tic 
-    X = zeros(p, n);
-    for k = 1:p
-        X(k, :) = x;
-    end
+tic
+X = zeros(p, n);
+for k = 1:p
+    X(k, :) = x;
+end
 toc
 
 tic
-    X = [];
-    for k = 1:p
-        X(k, :) = x;
-    end
+X = [];
+for k = 1:p
+    X(k, :) = x;
+end
 toc
 
 tic
-    X = [];
-    for k = p:-1:1
-        X(k, :) = x;
-    end
+X = [];
+for k = p:-1:1
+    X(k, :) = x;
+end
 toc
 
 %--------------------------------------------------------------------------
@@ -59,25 +60,78 @@ n = 30000;
 m = 150;
 
 tic
-    X = sparse(n, n);
-    for j = 1:m:n
-        for k = 1:m:n
-            X(j, k) = 1;
-        end
+X = sparse(n, n);
+for j = 1:m:n
+    for k = 1:m:n
+        X(j, k) = 1;
     end
+end
 toc
 
 [rows, cols] = find(X);
 
 tic
-    X = sparse(rows, cols, ones(size(rows)));
-    for j = 1:m:n
-        for k = 1:m:n
-            X(j, k) = 2;
-        end
+X = sparse(rows, cols, ones(size(rows)));
+for j = 1:m:n
+    for k = 1:m:n
+        X(j, k) = 2;
     end
+end
 toc
 
 tic
-    X = sparse(rows, cols, ones(size(rows)));
+X = sparse(rows, cols, ones(size(rows)));
+toc
+
+%% Using ()
+
+clc
+clear all
+
+n = 1000;
+v = randn(n, 1);
+a = randn(n, 1);
+loops = 100;
+
+tic
+for j = 1:loops
+    A = v * v';
+    y = A * a;
+end
+toc
+
+tic
+for j = 1:loops
+    y = v * v' * a;
+end
+toc
+
+tic
+for j = 1:loops
+    y = v * (v' * a);
+end
+toc
+
+%% Avoiding details
+
+clc
+clear all
+
+n = 100000;
+x = 0.5 - rand(n, 1);
+y = 0.5 - rand(n, 1);
+loops = 1000;
+
+tic
+for j = 1:loops
+    for k = 1:n
+        y(k) = 1.2345e-3 * y(k) + 2.2e-3 * x(k) + x(k) * y(k);
+    end
+end
+toc
+
+tic
+for j = 1:loops
+    y = 1.2345e-3 * y + 2.2e-3 * x + x .* y;
+end
 toc
