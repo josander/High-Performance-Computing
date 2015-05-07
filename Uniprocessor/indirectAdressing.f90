05c1fbc4f6c7d778d7790fa207a6e6cdb489497d
Josephine <jossan_cuellar@hotmail.com>
2015-05-07 19:16:28 +0200

    Started on indirectAdressing

--- Uniprocessor/indirectAdressing.f90
+++ Uniprocessor/indirectAdressing.f90
@@ -1,9 +1,18 @@
 program main
 
   implicit none 
-  integer           :: rows, cols, n, calls
-  double precision  :: temp
-  double precision 	:: fsecond, t, t1, time
+  integer           :: n
+  double precision 	:: a
+
+	double precision, dimension(1000000)	:: x, y
+	integer, dimension(1000000) ::	p
+
+	n = 1000000
+
+	do k = 1, n
+  	j = p(k)
+  	y(j) = y(j) + a * x(j)
+	end do
 
 
 end program main
