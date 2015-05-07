f543d5a4988ea8f37b0a3be1ea687089dea7e351
Josephine <jossan_cuellar@hotmail.com>
2015-05-07 16:35:00 +0200

    First task pretty much done

--- Uniprocessor/minTime.f90
+++ Uniprocessor/minTime.f90
@@ -5,104 +5,55 @@ program main
   implicit none 
   integer(8)	   :: k, n
   double precision :: fsecond, t, t1, t2, t3, t4, t5, cpuTime
-  double precision :: addMult, div, addition, sinFunk, expFunk
+  double precision :: expFunk
   real :: cput1, cput2 
 
 ! Arrays start at one by default.
-  double precision, dimension(4) :: add
-  double precision, dimension(1000000000) :: vec
+  double precision, dimension(100000000) :: vec
 
-  n = 1000000000
-  addMult = 0.0d0
-  div = 1.0d0
+  n = 100000000
 
 ! Initialize vec with ones
   do k = 1,n
-    vec(k) = 1.0d0
+    vec(k) = k * 0.00001d0
   end do
 
-! Print in terminal
+! Time for an addition----------------------------------------------------------
   print*, 'An addition operation was performed ', n * 4,' times.'
  
-! Time for an addition
-  add = addition(add, vec, n)
+  call additionTest(vec, n)
 
-! Print in terminal
   print*, '------------------------------------------------'
 
 
-! Time for an addition-multiplication pair
+! Time for an addition-multiplication pair--------------------------------------
 
-  t = fsecond()
-  call cpu_time(cput1)
+  print*, 'An addition-multiplication operation was performed ', n * 5,' times.'
 
-  do k = 1, n
-    addMult = addMult + 1.0 * 2.0
-  end do
-	
-  t2 = fsecond() - t
-  call cpu_time(cput2)
-  cpuTime = cput2 - cput1
+  call multAddTest(vec, n)
 
-  print*, 'An addition-multiplication operation was performed ', n,' times.'
-  print*, 'The result is ', addMult
-  print*, 'Time: ', t2
-  print*, 'CPU time: ', cpuTime
   print*, '------------------------------------------------'
 
-! Time for a division
-
-  t = fsecond()
-  call cpu_time(cput1)
-
-  do k = 1, n
-    div = div/1.01
-  end do
-	
-  t3 = fsecond() - t
-  call cpu_time(cput2)
-  cpuTime = cput2 - cput1
+! Time for a division-----------------------------------------------------------
 
   print*, 'A division operation was performed ', n,' times.'
-  print*, 'The result is ', div
-  print*, 'Time: ', t3
-  print*, 'CPU time: ', cpuTime
+
+  call divTest(vec, n)
+
   print*, '------------------------------------------------'
 
 ! Try different elementary functions
-
-  t = fsecond()
-  call cpu_time(cput1)
-
-  do k = 1, n
-    sinFunk = sin(dble(k))
-  end do
-	
-  t4 = fsecond() - t
-  call cpu_time(cput2)
-  cpuTime = cput2 - cput1
-
+  
   print*, 'A sine operation was performed ', n,' times.'
-  print*, 'The result is ', sinFunk
-  print*, 'Time: ', t4
-  print*, 'CPU time: ', cpuTime
+
+  call sinTest(vec, n)
+
   print*, '------------------------------------------------'
 
-  t = fsecond()
-  call cpu_time(cput1)
-
-  do k = 1, n
-    expFunk = exp(dble(k))
-  end do
-	
-  t5 = fsecond() - t
-  call cpu_time(cput2)
-  cpuTime = cput2 - cput1
-
   print*, 'A exponential operation was performed ', n,' times.'
-  print*, 'The result is ', expFunk
-  print*, 'Time: ', t5
-  print*, 'CPU time: ', cpuTime
+
+  call expTest(vec, n)
+
   print*, '------------------------------------------------'
 
 end program main
--- Uniprocessor/subMinTime.f90
+++ Uniprocessor/subMinTime.f90
@@ -1,8 +1,8 @@
-double precision function addition(add, vec, n)
+subroutine additionTest(vec, n)
 
   implicit none 
   integer	   :: k, n
-  double precision :: fsecond, t, t1, cpuTime
+  double precision :: fsecond, t, t1, cpuTime, time
   double precision, dimension(4) :: add
   double precision, dimension(n) :: vec
   real :: cput1, cput2 
@@ -12,18 +12,136 @@ double precision function addition(add, vec, n)
 
   do k = 1, n
     add(1) = 1.0d0 + vec(k)
-    add(2) = 1.0d0 + vec(k)
-    add(3) = 1.0d0 + vec(k)
-    add(4) = 1.0d0 + vec(k)
+    add(2) = 2.0d0 + vec(k)
+    add(3) = 3.0d0 + vec(k)
+    add(4) = 4.0d0 + vec(k)
   end do
 
   t1 = fsecond() - t
   call cpu_time(cput2)
   cpuTime = cput2 - cput1
+  time = t1 / (n * 4.0d0)
 
-  print*, 'Time per addition: ', t1 / (n * 4.0)
-  print*, 'CPU time: ', cpuTime / (n * 4.0)
+  print*, 'Time per addition: ', time
+  print*, 'CPU time: ', cpuTime / (n * 4.0d0)
+  print*, 'Gflop: ', 1 / (time * 10**9)
   print*, 'The variable equals', sum(add)
 
+end subroutine additionTest
 
-end function addition
+
+subroutine multAddTest(vec, n)
+
+  implicit none 
+  integer	   :: k, n
+  double precision :: fsecond, t, t1, cpuTime, time
+  double precision, dimension(5) :: multAdd
+  double precision, dimension(n) :: vec
+  real :: cput1, cput2 
+
+  t = fsecond()
+  call cpu_time(cput1)
+
+  do k = 1, n
+    multAdd(1) = 1.0d0 + vec(k) * 0.1d0
+    multAdd(2) = 2.0d0 + vec(k) * 0.2d0
+    multAdd(3) = 3.0d0 + vec(k) * 0.3d0
+    multAdd(4) = 4.0d0 + vec(k) * 0.4d0
+    multAdd(5) = 5.0d0 + vec(k) * 0.5d0
+  end do
+
+  t1 = fsecond() - t
+  call cpu_time(cput2)
+  cpuTime = cput2 - cput1
+  time = t1 / (n * 5.0d0)
+
+  print*, 'Time per addition: ', time
+  print*, 'CPU time: ', cpuTime / (n * 5.0d0)
+  print*, 'Gflop: ', 1 / (time * 10**9)
+  print*, 'The variable equals', sum(multAdd)
+
+end subroutine multAddTest
+
+
+subroutine divTest(vec, n)
+
+  implicit none 
+  integer	   :: k, n
+  double precision :: fsecond, t, t1, cpuTime, div, time
+  double precision, dimension(n) :: vec
+  real :: cput1, cput2 
+
+  t = fsecond()
+  call cpu_time(cput1)
+
+  do k = 1, n
+    div = 1.0d0 / vec(k)
+  end do
+
+  t1 = fsecond() - t
+  call cpu_time(cput2)
+  cpuTime = cput2 - cput1
+  time = t1 / (n * 1.0d0)
+
+  print*, 'Time per addition: ', time
+  print*, 'CPU time: ', cpuTime / (n * 1.0d0)
+  print*, 'Gflop: ', 1 / (time * 10**9)
+  print*, 'The variable equals', div
+
+end subroutine divTest
+
+subroutine sinTest(vec, n)
+
+  implicit none 
+  integer	   :: k, n
+  double precision :: fsecond, t, t1, cpuTime, sinT, time
+  double precision, dimension(n) :: vec
+  real :: cput1, cput2 
+
+  t = fsecond()
+  call cpu_time(cput1)
+
+  do k = 1, n
+    sinT = sin(vec(k))
+  end do
+
+  t1 = fsecond() - t
+  call cpu_time(cput2)
+  cpuTime = cput2 - cput1
+  time = t1 / (n * 1.0d0)
+
+  print*, 'Time per addition: ', time
+  print*, 'CPU time: ', cpuTime / (n * 1.0d0)
+  print*, 'Gflop: ', 1 / (time * 10**9)
+  print*, 'The variable equals', sinT
+
+end subroutine sinTest
+
+subroutine expTest(vec, n)
+
+  implicit none 
+  integer	   :: k, n
+  double precision :: fsecond, t, t1, cpuTime, expT, time
+  double precision, dimension(n) :: vec
+  real :: cput1, cput2 
+
+  t = fsecond()
+  call cpu_time(cput1)
+
+  do k = 1, n
+    expT = exp(-vec(k))
+  end do
+
+  t1 = fsecond() - t
+  call cpu_time(cput2)
+  cpuTime = cput2 - cput1
+  time = t1 / (n * 1.0d0)
+
+  print*, 'Time per addition: ', time
+  print*, 'CPU time: ', cpuTime / (n * 1.0d0)
+  print*, 'Gflop: ', 1 / (time * 10**9)
+  print*, 'The variable equals', expT
+
+end subroutine expTest
+
+
