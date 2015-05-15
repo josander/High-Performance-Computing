70b74e2e10ed6044c89c535ff9ec0b73e0ca0a0b
David <david.emil.svensson@gmail.com>
2015-05-15 17:59:54 +0200

    figures

--- Uniprocessor/matLapack.m
+++ Uniprocessor/matLapack.m
@@ -2,6 +2,7 @@
 clc
 clf
 clear all
+set(0,'defaulttextinterpreter','latex')
 
 % [n, time, nbrOperations, Gflops]
 refData = dlmread('RefData.txt');
@@ -11,21 +12,21 @@ oBlas2Data = dlmread('oBlas2TData.txt');
 oBlas3Data = dlmread('oBlas3TData.txt');
 oBlas4Data = dlmread('oBlas4TData.txt');
 lapac1Data = dlmread('lapackData.txt');
-lapacOB4Data = dlmread('lapackoBlas4TData.txt');
-%%
-plot(refData(:, 1), refData(:, 4),refData(:, 1), netlibData(:, 4),refData(:, 1), oBlas1Data(:, 4),refData(:, 1), oBlas2Data(:, 4),refData(:, 1), oBlas3Data(:, 4),refData(:, 1), oBlas4Data(:, 4),refData(:, 1), lapac1Data(:, 4),refData(:, 1), lapacOB4Data(:, 4))
-legend('Ref', 'Netlib', 'OB1','OB2','OB3','OB4', 'llapack1', 'llapackOB4')
+
 %% Nice plot
 
 set(gcf,'renderer','painters','PaperPosition',[-0.3 -0 16 12]);
 
-plot(data(:, 1), data(:, 2))
-x = xlabel('n');
-y = ylabel('Time');
+plot(refData(:, 1), refData(:, 4),refData(:, 1), netlibData(:, 4),refData(:, 1), oBlas1Data(:, 4),refData(:, 1), oBlas2Data(:, 4),refData(:, 1), oBlas3Data(:, 4),refData(:, 1), oBlas4Data(:, 4),refData(:, 1), lapac1Data(:, 4))
 
-title('Relative difference in signal');
-set(y, 'Units', 'Normalized', 'Position', [-0.085, 0.5, 0]);
-set(x, 'Units', 'Normalized', 'Position', [0.5, -0.085, 0]);
+x = xlabel('n');
+y = ylabel('Speed [Gflop/s] ');
+
+l = legend('Our \texttt{DPOTRF}', 'Netlib \texttt{DPOTRF}', 'OpenBlas 1 thread','OpenBlas 2 threads','OpenBlas 3 threads','OpenBlas 4 threads', 'Linux Lapack');
+title('Speed of \texttt{DPOTRF}');
+%set(y, 'Units', 'Normalized', 'Position', [-0.085, 0.5, 0]);
+%set(x, 'Units', 'Normalized', 'Position', [0.5, -0.15, 0]);
 plotTickLatex2D
+set(l,'interpreter', 'latex')
 print(gcf,'-depsc2','lapack.eps')
 
