e6e2694deb5e2e8bfda8fb9bcd53eb77a22baf0a
David <david.emil.svensson@gmail.com>
2015-05-15 17:37:01 +0200

    mums!

--- Uniprocessor/lapackTestOB.f90
+++ Uniprocessor/lapackTestOB.f90
@@ -14,48 +14,41 @@ program main
 	UPLO = 'L'
 
 ! Do the Cholesky factorization algorithm for different sizes of n
-	do n = 100, 5000, 100
-
-	! Initialize A as a symmetric and positive definite matrix
-		do k = 1, n
-			do i = 1, n
-				A(i, k) = 0.000001d0			
-			end do
-			A(k, k) = 1  
-		end do
-
-	! Take the time
-		t = fsecond()
-
-	! Cholesky factorization algorithm
-		do k = 1, 1 ! Change to n to run alorithm 
-
-			do i = 1, k - 1
-				sumA = sumA + A(k, i) * A(k, i)
-			end do		
-			A(k, k) = A(k, k) ** 0.5 - sumA
-			invDiag = 1.0d0 / A(k,k)
-			do j = k + 1, n
-
-
-				do i = 1, k -1
-					sumAA = sumAA + A(j, i) * A(k, i)
+	do n = 100, 2000, 100
+	
+		time = 0
+		do j = 1, 20
+		!	Reset A
+			do k = 1, n
+				do i = 1, n
+					A(i, k) = 0.000001d0			
 				end do
-				A(j, k) = ( A(j, k) - sumAA ) * invDiag
+				A(k, k) = 1  
 			end do
+
+		! Take the time
+			t = fsecond()
+
+			call dpotrf(UPLO, n, A, 5000, info)
+
+
+	! Do the same thing using the Lapack routine dpotrf
+
+			time = time + fsecond() - t
+	
 		end do
-		time = fsecond() - t
-		
+		time = time/20
 		nbrOperations = 2 *( n ** 2)  + 23 * n + 2
 		Gflops = nbrOperations / (time * 10.0d0**9)
+
+		open (unit = 2, file = "oBlas4TData.txt")
+		write (2, *) n, time, nbrOperations, Gflops
+		print*, 'NL Time: ', time
+		print*, n, sum(A), Gflops
 		
-
-
-		open (unit = 1, file = "RefDataTrash.txt")
-		write (1, *) n, time, nbrOperations, Gflops
-		print*, 'Time: ', time
-		print*, n, sum(A), Gflops 
-
+	end do
+	do n = 2100, 5000, 100
+	
 	!	Reset A
 		do k = 1, n
 			do i = 1, n
@@ -82,8 +75,8 @@ program main
 		print*, 'NL Time: ', time
 		print*, n, sum(A), Gflops
 		
-
 	end do	
+	
 
 	deallocate(A)
 	close (unit = 1)
--- Uniprocessor/matLapack.m
+++ Uniprocessor/matLapack.m
@@ -4,10 +4,17 @@ clf
 clear all
 
 % [n, time, nbrOperations, Gflops]
-data = dlmread('lapackData.txt');
-
-plot(data(:, 1), data(:, 2))
-
+refData = dlmread('RefData.txt');
+netlibData = dlmread('NLData.txt');
+oBlas1Data = dlmread('oBlas1TData.txt');
+oBlas2Data = dlmread('oBlas2TData.txt');
+oBlas3Data = dlmread('oBlas3TData.txt');
+oBlas4Data = dlmread('oBlas4TData.txt');
+lapac1Data = dlmread('lapackData.txt');
+lapacOB4Data = dlmread('lapackoBlas4TData.txt');
+%%
+plot(refData(:, 1), refData(:, 4),refData(:, 1), netlibData(:, 4),refData(:, 1), oBlas1Data(:, 4),refData(:, 1), oBlas2Data(:, 4),refData(:, 1), oBlas3Data(:, 4),refData(:, 1), oBlas4Data(:, 4),refData(:, 1), lapac1Data(:, 4),refData(:, 1), lapacOB4Data(:, 4))
+legend('Ref', 'Netlib', 'OB1','OB2','OB3','OB4', 'llapack1', 'llapackOB4')
 %% Nice plot
 
 set(gcf,'renderer','painters','PaperPosition',[-0.3 -0 16 12]);
