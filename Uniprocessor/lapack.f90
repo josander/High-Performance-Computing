6cfe9d1228ae732ea391194e3ddf1015a38fb9c7
David <david.emil.svensson@gmail.com>
2015-05-15 10:46:10 +0200

    ?

--- Uniprocessor/lapack.f90
+++ Uniprocessor/lapack.f90
@@ -15,11 +15,9 @@ program main
 
 ! Cholesky factorization algorithm
 	do k = 1, n
-
 		do i = 1, k - 1
 			sumA = sumA + A(k, i) * A(k, i)
 		end do		
-
 		A(k, k) = A(k, k)**0.5 - sumA
 
 		do j = k + 1, n
