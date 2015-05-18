f3f6f399e96270b243121ec27135ecc5e61c6ee4
Josephine <jossan_cuellar@hotmail.com>
2015-05-18 16:14:26 +0200

    Ngn har glömt att duscha här inne

--- Uniprocessor/mexFiles.m
+++ Uniprocessor/mexFiles.m
@@ -6,19 +6,48 @@ clear all
 mex -f ./mexopts.sh dgbmvTestMEX.c dgbmv.f -lblas
 
 A = [3 1 0 0; 1 2 1 0; 0 1 10 2; 0 0 1 6]
-x = [1 2 3 4]';
+klA = 1;
+kuA = 1;
+
+B = [2 2 1 1 0; 0 2 3 6 1; 0 0 5 4 1; 0 0 0 12 2; 0 0 0 0 9]
+klB = 0;
+kuB = 3;
+
+C = [2 2 1 1 0 0; 1 2 3 6 1 0; 1 1 5 4 1 1; 0 1 1 12 2 2; 0 0 1 1 9 1; 0 0 0 1 1 4]
+klC = 2;
+kuC = 3;
+
+x = [1 2 1 2]';
 y = zeros(4, 1);
-kl = 1;
-ku = 1;
 
-
+%%
 tic
 y = A * x
 toc
 
+% Try the MEX-function
+tic
+y = dgbmvTestMEX(klA, kuA, A, x)
+toc
+
+%%
+
+tic
+y = B * x
+toc
 
 % Try the MEX-function
 tic
-y = dgbmvTestMEX(kl, ku, A, x)
+y = dgbmvTestMEX(klB, kuB, B, x)
 toc
 
+%%
+
+tic
+y = C * x
+toc
+
+% Try the MEX-function
+tic
+y = dgbmvTestMEX(klC, kuC, C, x)
+toc
