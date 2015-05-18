879c7c06dc627cf8c28c92d7a27bdb4182fe5a17
David <david.emil.svensson@gmail.com>
2015-05-18 17:44:34 +0200

    fattar 0

--- Uniprocessor/mexFiles.m
+++ Uniprocessor/mexFiles.m
@@ -5,7 +5,7 @@ clear all
 
 mex -f ./mexopts.sh dgbmvTestMEX.c dgbmv.f -lblas
 
-A = [3 1 0 0; 1 2 1 0; 0 1 10 2; 0 0 1 6]
+A = [3 2 0 0; 1 2 1 0; 0 1 10 2; 0 0 1 6]
 x = [1 2 3 4]';
 y = zeros(4, 1);
 kl = 1;
