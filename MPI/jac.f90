7afd487378aa3f3da7d7f1ecb72ed97f324c7c11
Josephine Cuellar Andersson <josander@o7155a-07.o7155a.studat.rh6.ii.2.hb.s.cdal.chalmers.se>
2015-05-22 11:09:30 +0200

    ..

--- MPI/jac.f90
+++ MPI/jac.f90
@@ -4,51 +4,47 @@ program main
   include    "mpif.h"
   integer     message, length, source, dest, tag
   integer     my_rank, err
-  integer     n_procs    ! number of processes 
+  integer     n_procs  
   integer     status(MPI_STATUS_SIZE)
   integer 				:: n
   double precision, allocatable, dimension(:, :) :: U, f
 	double precision:: tau, delta 
 
-  call MPI_Init(err)  ! Start up MPI
+! Start up MPI
+  call MPI_Init(err)  
 
 ! Find out the number of n_processes and my rank 
   call MPI_Comm_rank(MPI_COMM_WORLD, my_rank, err)
   call MPI_Comm_size(MPI_COMM_WORLD, n_procs, err)
 
 
-  n = 10 !
+  n = 10 											! Gridsize
   allocate(f(0:n +1, 0:n +1)) ! (n + 2)^2 gridpoints
-  allocate(U(n,n)) ! (n)^2 gridpoints
+  allocate(U(n,n)) 						! (n)^2 gridpoints
 
-	tau = 0.1d0						 ! min error
-  call random_number(A)  ! work with A ...
-  print*, A(1, 1)        ! or whatever
-
-  call random_number(B)  ! work with B ...
-  print*, B(0, :)        ! or whatever
-  print*, B(n, 0)
-	
-	
+	tau = 0.1d0						 ! Minimum error
 
 	do while(delta >= tau)
 		delta = 0.01
-
 	end do
 
 
-	if ( my_rank == 0 ) then ! I'm the master process 
+! I'm the master process 
+	if ( my_rank == 0 ) then 
 
-  
-  else  ! I'm the slave process 
+
+! I'm the slave process 
+  else  
 
 
   end if
 
-  
-  call MPI_Finalize(err)  ! Shut down MPI 
 
+! Shut down MPI 
+  call MPI_Finalize(err)  
 
-  deallocate(B)          ! free the space
+! Free the space
+  deallocate(U)
+  deallocate(f)
 
 end program main
