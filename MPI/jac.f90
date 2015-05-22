5aca9979b479570cff55470230943d46660c51ee
Josephine Cuellar Andersson <josander@o7155a-07.o7155a.studat.rh6.ii.2.hb.s.cdal.chalmers.se>
2015-05-22 11:00:15 +0200

    Included the MPI-part

--- MPI/jac.f90
+++ MPI/jac.f90
@@ -1,13 +1,27 @@
-program allocprogram alloc
+program main
+
+  implicit none
+  include    "mpif.h"
+  integer     message, length, source, dest, tag
+  integer     my_rank, err
+  integer     n_procs    ! number of processes 
+  integer     status(MPI_STATUS_SIZE)
   integer 				:: n
   double precision, allocatable, dimension(:, :) :: U, f
 	double precision:: tau, delta 
+
+  call MPI_Init(err)  ! Start up MPI
+
+! Find out the number of n_processes and my rank 
+  call MPI_Comm_rank(MPI_COMM_WORLD, my_rank, err)
+  call MPI_Comm_size(MPI_COMM_WORLD, n_procs, err)
+
 
   n = 10 !
   allocate(f(0:n +1, 0:n +1)) ! (n + 2)^2 gridpoints
   allocate(U(n,n)) ! (n)^2 gridpoints
 
-	tau = 0.1d0							! min error
+	tau = 0.1d0						 ! min error
   call random_number(A)  ! work with A ...
   print*, A(1, 1)        ! or whatever
 
@@ -22,6 +36,19 @@ program allocprogram alloc
 
 	end do
 
+
+	if ( my_rank == 0 ) then ! I'm the master process 
+
+  
+  else  ! I'm the slave process 
+
+
+  end if
+
+  
+  call MPI_Finalize(err)  ! Shut down MPI 
+
+
   deallocate(B)          ! free the space
 
-end program alloc
+end program main
