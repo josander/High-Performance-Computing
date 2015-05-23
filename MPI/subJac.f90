8169ca190b364c810ce3a25c55a3277f88b9e970
Josephine Cuellar Andersson <josander@o7156-05.o7156.studat.rh6.ii.2.hb.s.cdal.chalmers.se>
2015-05-23 18:07:11 +0200

    seg fault

--- MPI/subJac.f90
+++ MPI/subJac.f90
@@ -162,7 +162,7 @@ subroutine initSPart(S, n, myRank)
 		S(j, xOff*( nHalf + 1 )) =(1 + xOff)*sin(xOff + y)
 
 		x = (j + nHalf*xOff )*h
-		S(yOff(nHalf + 1, j) = (1 + x)*sin(x + yOff) 
+		S(yOff*(nHalf + 1), j) = (1 + x)*sin(x + yOff) 
 	end do 	
 end subroutine initSPart
 
