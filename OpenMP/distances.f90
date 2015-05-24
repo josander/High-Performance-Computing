093ea0e279f03f97785bde9289dda1b44caaa58b
David <david.emil.svensson@gmail.com>
2015-05-24 14:51:46 +0200

    lol

--- OpenMP/distances.f90
+++ OpenMP/distances.f90
@@ -2,12 +2,28 @@ program main
 
   implicit none
 
-
+	integer						::			numCells, n, m, hashInd 					
+	double precision	::			distMax, distRes, dist
+	double precision, allocatable, dimension(:)		:: sumTable
+	integer, allocatable, dimension(:)						:: numTable
 ! Read file
 
 ! Create hash table
+	distMax = 70.0
+	distRes = 0.001
+
+
+	allocate(sumTable(INT((distMax - 1.0)/distRes + 1)) ! 69001 elemets
+	allocate(numTable(INT((distMax - 1.0)/distRes + 1))
 
 ! Paralell thing
+
+	do n = 1, numCells
+		do m = n + 1, numCells
+
+			dist = sqrt((cell1(1) - cell2(1))**2 + (cell1(2) - cell2(2))**2 +(cell1(3) - cell2(3))**2)
+
+	end do 
 
 ! Merge everything and compute the averages
 
