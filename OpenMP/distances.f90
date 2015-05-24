85563d982232dab201a06aafc56ada0b20cde042
Josephine Cuellar Andersson <josander@o7155b-06.o7155b.studat.rh6.ii.2.hb.s.cdal.chalmers.se>
2015-05-24 14:37:14 +0200

    Can read file

--- OpenMP/distances.f90
+++ OpenMP/distances.f90
@@ -1,15 +1,25 @@
 program main
 
   implicit none
+	integer 						:: i
+	double precision, dimension(3)		:: coordinates
 
 
 ! Read file
+	open(unit = 1, file = 'cells', action = 'read')
+
+	do i = 1, 5
+		read(1,*) coordinates
+		print*, coordinates
+	end do
 
 ! Create hash table
 
 ! Paralell thing
 
 ! Merge everything and compute the averages
+
+	close(unit = 1)
 
 
 end program main
