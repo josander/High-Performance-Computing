e84ffbd802b2bfe51fd169b503d15b2a791126f6
David <david.emil.svensson@gmail.com>
2015-05-26 16:21:08 +0200

    fixed openMP

--- OpenMP/distances.f90
+++ OpenMP/distances.f90
@@ -2,109 +2,69 @@ program main
 
 	implicit none
 	include 'omp_lib.h'
-	integer						::			numCells, n, m, hashInd, i,j, k, numUniq, sumUniq, blockSize					
+	integer						::			numCells,hashInd, i,j, numUniq, sumUniq			
 	double precision	::			distMax, distRes, dist,t ,t1,fsecond
 
-	double precision, allocatable, dimension(:)			:: distTable
+	double precision, dimension(3)									:: tempCell
 	integer, allocatable, dimension(:)							:: numTable
-	double precision, allocatable, dimension(:,:)		:: cell1, cell2
+	double precision, allocatable, dimension(:,:)		:: cell
 
 ! Read file
 	open(unit = 1, file = 'cells', action = 'read') 
-
-	distMax = 70.0
-	distRes = 0.001
-	numCells = 50000
-	blockSize = 5000 !numCells%blockSize must be 0 and numCells/blockSize >= numThreads  
+ 
+	distMax = 70.0 ! Maximum dist
+	distRes = 0.001	! Resolution of distances
+	numCells = 50000 ! Number of cells to be read from file (max 64000) 
 
 ! Allocate memory
-	allocate(distTable(INT(1/distRes) : INT((distMax)/distRes + 1))) 
 	allocate(numTable(INT(1/distRes) : INT((distMax)/distRes + 1)))
-	allocate(cell1(3, blockSize))
-	allocate(cell2(3, blockSize)) 	 	
+	allocate(cell(3, numCells))
+
 
 ! Initialize the hash table
 	do i = INT(1/distRes), INT((distMax)/distRes + 1)
-		distTable(i) = 0.0
 		numTable(i) = 0
 	end do
 
+	do i = 1, numCells
+		read(1,*), cell(1,i), cell(2,i), cell(3,i)
+	end do		
 
-  t = fsecond()
+!  t = fsecond() ! for debugging and testing
 
-! Paralell thing
-
-!$OMP parallel do private(n, m, hashInd, dist, k, i, j,  cell1, cell2) REDUCTION( + : distTable, numTable)
-	do j = 1, numCells  , blockSize
-		
-!$OMP CRITICAL 
-		rewind 1
-		do i = 1, j - 1
-			read(1,*) 
-		end do
-		do i = 1, blockSize
-			read(1,*) cell1(1,i), cell1(2,i), cell1(3,i) 
-		
-		end do		
-!$OMP END CRITICAL 
-	
-		!Evaluate the first block
-		do n = 1, blockSize
-			do m = n+ 1 , blockSize
-			dist = sqrt((cell1(1,n) - cell1(1,m))**2 + (cell1(2,n) - cell1(2,m))**2 +(cell1(3,n) - cell1(3,m))**2)
-			hashInd = INT(1000*dist)
-		
-			distTable(hashInd) = distTable(hashInd) + dist
-			numTable(hashInd) =  numTable(hashInd) + 1 
-	
-			end do
-		end do
-
-		do k = blockSize + j , numCells , blockSize
-!$OMP CRITICAL 
-			rewind 1
-			do i = 1, k - 1
-				read(1,*) 
-			end do
-			do i = 1, blockSize
-				read(1,*) cell2(1,i), cell2(2,i), cell2(3,i)
-			end do		
-!$OMP END CRITICAL 
-
-			do n = 1, blockSize
-				do m = 1 , blockSize
-				!print*, cell1(1,n), cell2(1,m)			
-				dist = sqrt((cell1(1,n) - cell2(1,m))**2 + (cell1(2,n) - cell2(2,m))**2 +(cell1(3,n) - cell2(3,m))**2)
-				hashInd = INT(1000*dist)
-
-				distTable(hashInd) = distTable(hashInd) + dist
+! Paralell region
+!$OMP parallel do private(hashInd, dist,  i, j, tempCell) REDUCTION( + : numTable) schedule(dynamic)
+	do j = 1, numCells 			
+		tempCell = cell(:,j)
+		do i = j+1, numCells
+				dist = sqrt((tempCell(1) - cell(1,i))**2 + (tempCell(2) - cell(2,i))**2 +(tempCell(3) - cell(3,i))**2)
+				hashInd = INT(1000*dist )
 				numTable(hashInd) =  numTable(hashInd) + 1 
-	
-				end do
-			end do
-		end do		
-	end do
+		end do
+	end do 	
+
 !$OMP end parallel do 
+
+!	t1 = fsecond() - t	
+!	print*,"Time:", t1 
 
 ! Compute the averages and write to file
 	numUniq = 0 
 	sumUniq	= 0
 
   open (unit=22,file="results.txt",action="write",status="replace")
-
+! Print the number of times each distance occurs
 	do i = INT(1/distRes), INT((distMax)/distRes + 1)
 		if(numTable(i) > 0) then		
-			distTable(i) = distTable(i)/numTable(i)
 			numUniq = numUniq + 1 
 			sumUniq = sumUniq + numTable(i)
-			write (22,*), distTable(i), numTable(i)  
+			write (22,'(F6.3,i10)'), i/1000.0, numTable(i)
 		end if
 	end do
-  
-	write (22,*),"numUniq: ", numUniq,"sumUniq: ", sumUniq  
+! Print the number of unique distances and the total number of computed distances  
+	write (22,*),"numUniq: ", numUniq,"sumUniq: ", sumUniq 
+! Note that each pair of distances only is counted once since d(a,b) = d(b,a)
 
-	t1 = fsecond() - t	
-	print*,"Time:", t1 
 
 ! Close units
  	close (unit =22)
